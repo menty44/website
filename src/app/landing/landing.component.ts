8537554a54af0184cc89c5f80fcc4ef122d18e60
Fredrick Oluoch <menty44@gmail.com>
2024-12-23 10:29:44 +0300

    List and Grid default design

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ListComponent } from "../list/list.component";
+import { GridComponent } from "../grid/grid.component";
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [ListComponent, GridComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
